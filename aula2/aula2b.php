a046862fceba2a2cd1b1711375d07b3d773739b9
wiliamcgalvao <72823475+wiliamcgalvao@users.noreply.github.com>
2024-04-09 10:24:56 -0300

    Add files via upload

---
+++ aula2/aula2b.php
@@ -0,0 +1,11 @@
+<?php
+$nome = "Wiliam";
+$sobrenome = "Galvão";
+  
+echo ("Nome: ".$nome. "<br>");  
+echo ("Sobrenome: ".$sobrenome."<br>"); 
+
+echo ("Nome Completo: ". $nome ." ". $sobrenome . "<br>" );
+   
+echo "<h2>PHP is Fun!</h2>";
+?>
