18462a3c050caf87a8d6cb3d1acdb4902d6201f4
wiliamcgalvao <72823475+wiliamcgalvao@users.noreply.github.com>
2024-04-09 10:36:53 -0300

    Add files via upload

---
+++ aula5/aula5.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello" . "<br>";
+echo 'Hello' . '<br>';
+echo 'Wiliam' . '<br>';
+$x = "Wiliam";
+echo "Hello $x" . '<br>';
+echo 'Hello $x' . '<br>';
+
+echo strlen("Wiliam Galvão") . "<br>";
+echo str_word_count("Hello world! Prova") . "<br>";
+
+echo str_word_count('A proporção de brasileiros com acesso à rede de coleta e tratamento de esgoto cresceu de 52,8% em 2010 para 62,5% em 2022, mas 75,8 milhões de pessoas (37,5%) ainda vivem desconectadas do sistema… - Veja mais em ') . "<br>";
+echo str_word_count("A proporção de brasileiros com acesso à rede de coleta e tratamento de esgoto cresceu de 52,8% em 2010 para 62,5% em 2022, mas 75,8 milhões de pessoas (37,5%) ainda vivem desconectadas do sistema… - Veja mais em ") . "<br>";
+
+
+echo strpos("Hello ou world jose!", "jose");
+
+echo ("<br>");
+//echo... contar letras....
+echo strlen("A proporção de brasileiros com acesso à rede de coleta e tratamento de esgoto cresceu de 52,8% em 2010 para 62,5% em 2022, mas 75,8 milhões de pessoas (37,5%) ainda vivem desconectadas do sistema… - Veja mais em ") . "<br>";
+
+
+?>
